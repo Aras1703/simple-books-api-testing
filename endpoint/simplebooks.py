fb57a5e940772c6e2ce86419ce271842ae12414e
Arasy Bazwir <83108505+Aras1703@users.noreply.github.com>
2023-05-18 13:51:53 +0700

    first commit

---
+++ endpoint/simplebooks.py
@@ -0,0 +1,47 @@
+from endpoint.base import APIClient
+
+class SimpleBooksAPI(APIClient):
+    def __init__(self):
+        super().__init__()
+        self.base_url = 'https://simple-books-api.glitch.me'
+    
+    def get_status(self):
+        return self.get(self.base_url+'/status')
+    
+    def regist_api_key(self, Name, Email):
+        payload = {
+            'clientName':Name,
+            'clientEmail':Email,
+        }
+        return self.post(self.base_url+'/api-clients', data=payload)
+    
+    def get_list_books(self):
+        return self.get(self.base_url+'/books')
+    
+    def get_single_book(self, booksId):
+        return self.get(f'{self.base_url}/{booksId}')
+    
+    def order_book(self, bookId, customerName, api_key):
+        payload = {
+            'bookId':bookId,
+            'customerName':customerName,
+        }
+        headers = {
+            'Authorization':api_key,
+        }
+        return self.post(self.base_url+'/orders', data=payload, headers=headers)
+    
+    def get_all_book_orders(self, api_key):
+        headers = {
+            'Authorization':api_key,
+        }
+        return self.get(self.base_url+'/orders', headers=headers)
+    
+    def get_ordered_book(self, orderId, api_key):
+        headers = {
+            'Authorization':api_key,
+        }
+        return self.get(f'{self.base_url}/orders/:{orderId}', headers=headers)
+    
+    def update_order(self, orderId, customerName):
+        pass
