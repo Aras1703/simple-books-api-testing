142c93ee5afd43a6c15cf1a1e98eda6977f3c96b
Arasy Bazwir <83108505+Aras1703@users.noreply.github.com>
2023-05-22 00:19:10 +0700

    test status code

---
+++ endpoint/test_simple_books.py
@@ -0,0 +1,13 @@
+import pytest
+from simple_books import SimpleBooksAPI
+
+@pytest.fixture()
+def simplebooks_api():
+    return SimpleBooksAPI()
+
+
+def test_get_status(simplebooks_api):
+    response = simplebooks_api.get_status()
+    assert response.status_code == 200
+    
+    
